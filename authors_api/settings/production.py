00e58aef986784172b298cb291b2890b7378ba08
edorich <hendro.hudaya@gmail.com>
2023-08-09 14:44:36 +0700

    django settings

---
+++ authors_api/settings/production.py
@@ -0,0 +1,8 @@
+from .base import * #noqa
+from .base import env
+
+ADMINS = [("Hendro Hudaya", "hendro.hudaya@gmail.com")]
+
+# TODO: add domain names of the production server
+
+CSRF_TRUSTED_ORIGINS = [""]
