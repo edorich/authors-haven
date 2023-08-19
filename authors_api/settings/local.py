b4f2e7c514a22a4494b06f80616cf7c3428a81a0
edorich <hendro.hudaya@gmail.com>
2023-08-19 13:42:51 +0700

    configure celery, redis, flower

--- authors_api/settings/local.py
+++ authors_api/settings/local.py
@@ -8,3 +8,10 @@ SECRET_KEY = env("DJANGO_SECRET_KEY",default="KT_QXTKa86MoIMrTMnF6JyOuUrEatQ2pYu
 DEBUG = True
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = "support@apiimperfect.site"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Author Haven"
